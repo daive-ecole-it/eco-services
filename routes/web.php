d5e20a824fafcca6315900271f8fad06ded1c90e
MasterCode20 <juniorngoumou724@gmail.com>
2024-07-04 07:24:31 +0200

    add my products and category tables

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\HomeController;
 Route::get('/',[HomeController::class, 'home']);
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('home.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
